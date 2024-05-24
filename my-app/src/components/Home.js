c2c6d4b594a32591cb0d9e39e7c5e7c2e6287e9d
Andrew Diaz <andrewdiazn20@gmail.com>
2024-05-23 20:07:03 -0700

    fourth-commit

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -3,7 +3,20 @@ import React from "react"
 function Home(){
     return(
         <main>
-            <h1>Arc-Performance-Training</h1>
+            <div>
+                <h1>ARC Performance Training</h1>
+                <p>Welcome Students!</p>
+                <button>Sign a Waiver</button>
+            </div>
+            <div>
+                <h1>Services</h1>
+            </div>
+            <div>
+                <h1>Upcoming Classes</h1>
+            </div>
+            <div>
+                <h1>About page</h1>
+            </div>
         </main>
     )
 }
