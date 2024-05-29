bb563874899d80de03611787ee7c801c70b22ce5
Andrew Diaz <andrewdiazn20@gmail.com>
2024-05-29 15:08:31 -0700

    Services

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -12,14 +12,22 @@ function Home() {
                     <p>Welcome Students!</p>
                 </div>
             </main>
-            <div className="ServicesCss"/>
-            <h1>Services</h1>
+            <div>
+            <h1 className="ServCss">Services</h1>
+            </div>
             <section className='homecss2'>
-                <div>
+                <div className="upBox">
                     <h1>Upcoming Classes</h1>
+                    <h3>Tuesday</h3>
+                    <p>rock-climbing: 5:00-7:00</p>
+                    <h3>Wenseday</h3>
+                    <p>Gym: 12:00-3:00</p>
+                    <h3>Thursday</h3>
+                    <p>Hiking: 12:00-3:00</p>
                 </div>
-                <div>
+                <div className="upBox">
                     <h1>About page</h1>
+                    <p>dufhgDPIFUHdSVNUDSv</p>
                 </div>
             </section>
             <section>
